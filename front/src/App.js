3bb335e7e875e159269513b6e5d227a5e8c979d2
Mouad <devmahfoud@gmail.com>
2021-01-24 00:01:56 +0100

    backend part

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,40 @@
+import React, { useEffect, useState } from "react";
+import Pusher from "pusher-js";
 import "./App.css";
 import Chat from "./Chat";
 import Sidebar from "./Sidebar";
+import axios from "./axios";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/messages/sync").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    const pusher = new Pusher("c40500535d3db6f51f7a", {
+      cluster: "eu",
+    });
+
+    const channel = pusher.subscribe("messages");
+    channel.bind("inserted", (newMessage) => {
+      setMessages([...messages, newMessage]);
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
+
   return (
     <div className="app">
       <div className="app__body">
         <Sidebar />
-        <Chat />
+        <Chat messages={messages} />
       </div>
     </div>
   );
--- front/src/Chat.js
+++ front/src/Chat.js
@@ -6,10 +6,25 @@ import {
   InsertEmoticon,
   Mic,
 } from "@material-ui/icons/index";
-import React from "react";
+import React, { useState } from "react";
+import axios from "./axios";
 import "./Chat.css";
 
-const Chat = () => {
+const Chat = ({ messages }) => {
+  const [input, setInput] = useState("");
+
+  const sendMessage = async (event) => {
+    event.preventDefault();
+
+    await axios.post("/api/messages/new", {
+      message: input,
+      name: "momo",
+      timestamp: new Date().toLocaleDateString,
+      received: false,
+    });
+    setInput("");
+  };
+
   return (
     <div className="chat">
       <div className="chat__header">
@@ -33,27 +48,29 @@ const Chat = () => {
         </div>
       </div>
       <div className="chat__body">
-        <p className="chat__message">
-          <span className="chat__name">Momo</span>
-          My awesome message
-          <span className="chat__timestamp">{new Date().toLocaleString()}</span>
-        </p>
-        <p className="chat__message chat__receiver">
-          <span className="chat__name">Momo</span>
-          My awesome message
-          <span className="chat__timestamp">{new Date().toLocaleString()}</span>
-        </p>
-        <p className="chat__message">
-          <span className="chat__name">Momo</span>
-          My awesome message
-          <span className="chat__timestamp">{new Date().toLocaleString()}</span>
-        </p>
+        {messages.map((message) => (
+          <p
+            className={`chat__message ${message.received && "chat__receiver"}`}
+          >
+            <span className="chat__name">{message.name}</span>
+            {message.message}
+            <span className="chat__timestamp">{message.timestamp}</span>
+          </p>
+        ))}
       </div>
+
       <div className="chat__footer">
         <InsertEmoticon />
         <form>
-          <input type="text" placeholder="Type a message" />
-          <button type="submit">send</button>
+          <input
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+            type="text"
+            placeholder="Type a message"
+          />
+          <button onClick={sendMessage} type="submit">
+            send
+          </button>
         </form>
         <Mic />
       </div>
